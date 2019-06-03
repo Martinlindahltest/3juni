5a70ebc55bb2f9d18e4678a02adadcfcb4b63c0b
Martinlindahltest <basmartin@gmail.com>
2019-06-03 14:51:25 +0200

    border-boxar

--- src/App.js
+++ src/App.js
@@ -6,39 +6,24 @@ import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
     <div className="App">
-<div class="card-columns">
-  <div class="card bg-primary">
-    <div class="card-body text-center">
-      <p class="card-text">Some text inside the first card</p>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm border">
+      One of three columns
+    </div>
+    <div class="col-sm border" >
+      One of three columns
+    </div>
+    <div class="col-sm border">
+      One of three columns
+    </div>
+    <div class="col-sm border">
+      One of three columns
     </div>
   </div>
-  <div class="card bg-warning">
-    <div class="card-body text-center">
-      <p class="card-text">Some text inside the second card</p>
-    </div>
-  </div>
-  <div class="card bg-success">
-    <div class="card-body text-center">
-      <p class="card-text">Some text inside the third card</p>
-    </div>
-  </div>
-  <div class="card bg-danger">
-    <div class="card-body text-center">
-      <p class="card-text">Some text inside the fourth card</p>
-    </div>
-  </div> 
-  <div class="card bg-light">
-    <div class="card-body text-center">
-      <p class="card-text">Some text inside the fifth card</p>
-    </div>
-  </div>
-  <div class="card bg-info">
-    <div class="card-body text-center">
-      <p class="card-text">Some text inside the sixth card</p>
-    </div>
-  </div>
+</div>     
 </div>
-    </div>
   );
 }
 
