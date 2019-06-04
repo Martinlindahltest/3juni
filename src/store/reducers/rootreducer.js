5155c0db5de3e8c20cabd0db3f91843d13449266
Martinlindahltest <basmartin@gmail.com>
2019-06-04 23:53:47 +0200

    ny disp

---
+++ src/store/reducers/rootreducer.js
@@ -0,0 +1,13 @@
+const initState  = {
+    posts: [
+        {id: 1, title: 'tata2', body: 'baba 3'},
+        {id: 2, title: 'tata2', body: 'baba 3'},
+        {id: 3, title: 'tata2', body: 'baba 3'}
+    ]
+}
+
+const rootReducer = (state = initState, action) => {
+    return state
+}
+
+export default rootReducer
