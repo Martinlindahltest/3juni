4667f07503373b4663c87dd65a380d9af6640e64
Martinlindahltest <basmartin@gmail.com>
2019-06-04 14:38:54 +0200

    test dispach fungerar

--- src/components/Testredux.js
+++ src/components/Testredux.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { connect } from 'react-redux'
 
  function Testredux(props) {
-     console.log(props.posts)
+     console.log(props)
+     props.deletePosts('från testredux')
     return (
         <div>
             testredux
@@ -14,7 +15,15 @@ const mapStateToProps = (state) => ({
     posts: state.posts
 })
 
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePosts: (input) => {
+            dispatch({type: 'CONSOLE_LOG', payload: input})
+        }
+    }    
+}
 
 
 
-export default connect(mapStateToProps)(Testredux)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Testredux)
